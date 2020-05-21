97347c26e6e1dfa801e80bf3048a4e47b7a47902
areong9292 <areong9292@gmail.com>
2020-05-22 00:00:54 +0900

    Edit iasTest

--- KotlinProject/app/src/main/java/com/example/kotlinproject/Step2/iasTest.kt
+++ KotlinProject/app/src/main/java/com/example/kotlinproject/Step2/iasTest.kt
@@ -5,29 +5,29 @@ import com.example.kotlinproject.TestClass
 class iasTest (p : (Any)->Unit) : TestClass(p) {
     override fun doTest() {
         var WhatYouWant = 2
-        var inter : A = if(WhatYouWant == 1) B2() else B()
+        var inter : InterTest = if(WhatYouWant == 1) InterImp2() else InterImp()
 
         interfaceTest(inter)
 
-        var obj2 = D();
+        var obj2 = TestAbstractImp();
         obj2.TestFunc()
         obj2.abstractFunc()
 
-        println(D.staticVar)
-        D.staticFunc()
+        println(TestAbstractImp.staticVar)
+        TestAbstractImp.staticFunc()
     }
 
-    fun interfaceTest(pInter : A) {
-        pInter.doA()
+    fun interfaceTest(pInter : InterTest) {
+        pInter.TestFunc()
     }
 
     // 이 메소드를 상속받는 애는 무조건 doA를 구현해야한다
-    interface A{
-        fun doA()
+    interface InterTest{
+        fun TestFunc()
     }
 
-    class B : A {
-        override fun doA() = println("A InterTest")/* {
+    class InterImp : InterTest {
+        override fun TestFunc() = println("InterImp InterTest")/* {
             // ctrl + o 를 누르면 interface의 메소드 선택해서 override된다
             // 그리고 안에는 아래의 TODO가 생성됨
             // 이거를 그대로 두면 앱이 TODO를 만나면 종료되서 실행 안되고 지워줘야함
@@ -37,8 +37,8 @@ class iasTest (p : (Any)->Unit) : TestClass(p) {
         }*/
     }
 
-    class B2 : A{
-        override fun doA() = println("A2")/* {
+    class InterImp2 : InterTest{
+        override fun TestFunc() = println("InterImp InterTest2")/* {
             println("A2")
         }*/
     }
@@ -48,15 +48,15 @@ class iasTest (p : (Any)->Unit) : TestClass(p) {
         abstract fun abstractFunc()
     }
 
-    class D : TestAbstract() {
+    class TestAbstractImp : TestAbstract() {
         // abstract로 선언했기에 반드시 override로 구현해야한다
-        override fun abstractFunc() {
+        override fun abstractFunc() = println("TestAbstractImp abstractFunc")/*{
             //TODO("Not yet implemented")
-        }
+        }*/
 
         companion object {
             var staticVar = "companion staticVar"
-            fun staticFunc() = println(staticVar)
+            fun staticFunc() = println("companion staticFunc")
         }
     }
 }
